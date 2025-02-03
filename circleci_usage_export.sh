ae3546d988e4db2201b76cf7e389599f426cab68
Henna Abbas <henna@circleci.com>
2025-02-02 19:27:57 -0800

    removed commented out code

--- circleci_usage_export.sh
+++ circleci_usage_export.sh
@@ -67,56 +67,6 @@ else
 fi
 
 
-# # Make the first API call to create the usage export job
-# usage_export_job_id=$(curl --silent --request POST \
-#   --url "https://circleci.com/api/v2/organizations/${ORG_ID}/usage_export_job" \
-#   --header "Circle-Token: ${CIRCLE_TOKEN}" \
-#   --header 'Content-Type: application/json' \
-#   --data "{\"start\":\"${START_DATE}\",\"end\":\"${END_DATE}\",\"shared_org_ids\":[\"${ORG_ID}\"]}" | jq -r .usage_export_job_id)
-
-# # Check if the job ID was retrieved successfully
-# if [ -z "$usage_export_job_id" ]; then
-#   echo "Failed to create usage export job."
-#   exit 1
-# fi
-
-# echo "Usage export job created with ID: $usage_export_job_id"
-
-# # Initialize variables for polling
-# max_attempts=10
-# attempt=0
-# job_state="processing"
-
-# # Poll for job status until it's no longer "processing" or max attempts reached
-# while [[ "$job_state" == "processing" && $attempt -lt $max_attempts ]]; do
-#   # Make the second API call to get the status of the usage export job
-#   job_status=$(curl --silent --request GET \
-#     --url "https://circleci.com/api/v2/organizations/${ORG_ID}/usage_export_job/${usage_export_job_id}" \
-#     --header "Circle-Token: ${CIRCLE_TOKEN}")
-
-#   # Output the job status
-#   echo "Job status:"
-#   echo "$job_status" | jq
-
-#   # Extract the value of the 'state' key
-#   job_state=$(echo "$job_status" | jq -r .state)
-#   echo "Job state: $job_state"
-
-#   # Increment the attempt counter
-#   attempt=$((attempt + 1))
-
-#   # Sleep for a while before the next check
-#   if [[ "$job_state" == "processing" ]]; then
-#     echo "Job is still processing. Waiting for 30 seconds before checking again..."
-#     sleep 30
-#   fi
-# done
-
-# if [[ "$job_state" == "processing" ]]; then
-#   echo "Max attempts reached. Job is still processing."
-# else
-#   echo "Job has finished with state: $job_state"
-# fi
 
 
 
